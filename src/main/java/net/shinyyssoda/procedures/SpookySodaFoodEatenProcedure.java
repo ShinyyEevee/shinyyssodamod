62abbb0d1ace6bc2bd724955f155a3cee0a59bac
ShinyyEevee <ShinyyEevee@192.168.0.76>
2020-10-31 01:15:09 -0400

    Spooky Scary Skeletons
    (Halloween Update: Spooky Soda Added, kill Husks to get.)

---
+++ src/main/java/net/shinyyssoda/procedures/SpookySodaFoodEatenProcedure.java
@@ -0,0 +1,46 @@
+package net.shinyyssoda.procedures;
+
+import net.shinyyssoda.SodaModElements;
+
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import java.util.Map;
+
+@SodaModElements.ModElement.Tag
+public class SpookySodaFoodEatenProcedure extends SodaModElements.ModElement {
+	public SpookySodaFoodEatenProcedure(SodaModElements instance) {
+		super(instance, 183);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			System.err.println("Failed to load dependency entity for procedure SpookySodaFoodEaten!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		if ((Math.random() < 0.25)) {
+			if (entity instanceof LivingEntity)
+				((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.SPEED, (int) 5000, (int) 1, (false), (false)));
+		} else if ((Math.random() < 0.25)) {
+			if (entity instanceof LivingEntity)
+				((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.STRENGTH, (int) 5000, (int) 1, (false), (false)));
+		} else if ((Math.random() < 0.25)) {
+			if (entity instanceof LivingEntity)
+				((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.BLINDNESS, (int) 5000, (int) 1, (false), (false)));
+			if (entity instanceof LivingEntity)
+				((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.SLOWNESS, (int) 5000, (int) 1, (false), (false)));
+			if (entity instanceof LivingEntity)
+				((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.NAUSEA, (int) 5000, (int) 1, (false), (false)));
+		} else if ((Math.random() < 0.1)) {
+			if (entity instanceof LivingEntity)
+				((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.REGENERATION, (int) 5000, (int) 1, (false), (false)));
+			if (entity instanceof LivingEntity)
+				((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.ABSORPTION, (int) 5000, (int) 1, (false), (false)));
+			if (entity instanceof LivingEntity)
+				((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.LUCK, (int) 5000, (int) 1, (false), (false)));
+		}
+	}
+}
